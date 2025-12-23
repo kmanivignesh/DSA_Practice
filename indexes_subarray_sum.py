ec8b63a8861dc76e1d607ac80fc319b9bf34f00e
Mani Vignesh <kmvignesh2005@gmail.com>
2025-12-23 19:20:38 +0530

    targte_subarray_sum

---
+++ indexes_subarray_sum.py
@@ -0,0 +1,21 @@
+#Amazon , Facebook ,Google , Visa
+
+def subarray_sum(arr , target):
+    i = 0
+    sum = arr[0]
+    for j in range(1,len(arr)):
+        
+        while sum >= target:
+            sum -= arr[i]
+            i += 1
+            if sum == target:
+                return i,j
+        sum+=arr[j]
+    return -1
+
+arr = [1 , 2 , 3 , 7 , 5]
+target = 12
+print(subarray_sum(arr , target))
+
+
+
