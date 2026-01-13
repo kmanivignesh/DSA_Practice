c9c2d0b7364ef72cbbaf2359359981fcef389c59
Mani Vignesh <kmvignesh2005@gmail.com>
2026-01-13 20:27:32 +0530

    LL , add , delete , length

---
+++ Linked_List/linked_list.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self , data):
+        self.value = data
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_node(self , data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = new_node
+
+    def delete_node(self):
+        if self.head is None:
+            print("Can not delete")
+        elif self.head.next is None:
+            self.head = None 
+        else:
+            temp = self.head
+            prev = temp
+            while temp.next is not None :
+                prev = temp
+                temp = temp.next
+            prev.next = None
+
+    def print_ll(self):
+        if self.head is  None:
+            print("Linked list empty")
+        else:
+            temp = self.head
+            while temp is not None:
+                print(temp.value ,end  = ' ')
+                temp = temp.next
+            print()  
+
+    def find_length(self):
+        if self.head is None:
+            print("Linked list is Empty")
+        else:
+            temp = self.head
+            count = 0
+            while temp is not None:   
+                count+=1
+                temp = temp.next
+            print("The length of the linked list is ",count)   
+
+                     
+
+
+l1 = LinkedList()
+l1.add_node(30)
+l1.add_node(40)
+l1.add_node(50) 
+l1.find_length()  
+l1.print_ll()
+l1.delete_node()
+l1.print_ll()
+l1.find_length()
+
